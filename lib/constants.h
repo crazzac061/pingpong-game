f398a268573b8a5a5410c07f2d6bde78e4521ed7
[Bini Chand] <[079bct0b33.bini@pcampus.edu.np]>
2024-11-25 19:51:49 +0545

    constant lib

---
+++ lib/constants.h
@@ -0,0 +1,14 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <raylib.h>
+
+extern const Color Green;
+extern const Color Dark_Green;
+extern const Color Light_Green;
+extern const Color Yellow;
+
+extern int player1_score;
+extern int player2_score;
+
+#endif // CONSTANTS_H
