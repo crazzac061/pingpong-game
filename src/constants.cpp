a0bd936dbe88c465816dcc4143d4d626de925a7d
[Bini Chand] <[079bct0b33.bini@pcampus.edu.np]>
2024-11-26 22:07:47 +0545

    added constant

---
+++ src/constants.cpp
@@ -0,0 +1,9 @@
+#include <lib/Constants.h>
+
+const Color Green = { 38, 185, 154, 255 };
+const Color Dark_Green = { 20, 160, 133, 255 };
+const Color Light_Green = { 129, 204, 184, 255 };
+const Color Yellow = { 243, 213, 91, 255 };
+
+int player1_score = 0;
+int player2_score = 0;
